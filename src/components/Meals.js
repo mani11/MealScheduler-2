9b451bbd9961a8d71d0d39da5cb841e2ee60fad0
Manleen Bhatia <manleenbhatia@Manleens-MacBook-Pro.local>
2019-11-04 23:53:58 -0800

    adding unique keys to the list items in Meals.js

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -11,16 +11,16 @@ const Meals = ((props) => {
 
   Array.from(props.mealsSchedule).map(([date,guests]) => {
     breakfastDates = guests.map((g,index)=>{
-         return <li key={index} className="morning">Breakfast for {g} on {date}</li>
+         return <li key={'morning'+index+g+date} className="morning">Breakfast for {g} on {date}</li>
     });
 
     lunchDates = guests.map((g,index)=>{
-       return <li key={index} className="afternoon">Lunch for {g} on {date}</li>
+       return <li key={'noon'+index+g+date}  className="afternoon">Lunch for {g} on {date}</li>
 
     });
 
     dinnerDates = guests.map((g,index)=>{
-       return <li key={index} className="night">Dinner for {g} on {date}</li>
+       return <li key={'night'+index+g+date} className="night">Dinner for {g} on {date}</li>
     });
     listItems = listItems.concat(breakfastDates.concat(lunchDates).concat(dinnerDates));
   });
